7796ad6367baa74fd6276471bc0687238d84dda3
luis-luciqm <redbullfc18@gmail.com>
2021-05-01 10:28:13 -0300

    Iniciando no NodeJS

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => { // Array function
-    res.send("Iniciando API com nodeJS")
+    // res.send("Iniciando API com nodeJS")
+    return res.json({
+        titulo: "Como criar uma API" // retornando um objeto de dados
+    })
 })
 
 app.listen(8080, (req, res) => {
